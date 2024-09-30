901bc1f40e6a2be0dffad1527a3b1a594e53cab0
Vaiko Villiam Tuul <vaikotuul@users.noreply.github.com>
2024-09-30 11:52:27 +0300

    EJS view engine kasutusele võetud

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const express = require("express");
+const dateTime = require("./dateTime");
+const fs = require("fs");
 const app = express();
 
+//määran view mootori
+app.set("view engine", "ejs");
+//määran jagatavate failide kausta
+app.use(express.static("public"));
+
 app.get("/", (req, res)=>{
-	res.send("Express läks täitsa käima!");
+	//res.send("Express läks täitsa käima!");
+	res.render("index");
 });
 
-app.listen(5135);
+app.get("/timenow", (req, res)=>{
+	const weekdayNow = dateTime.weekDayEt();
+	const dateNow = dateTime.dateFormattedEt();
+	const timeNow = dateTime.timeFormattedEt();
+	res.render("timenow", {nowWD: weekdayNow, nowD: dateNow, nowT: timeNow});
+});
+
+app.get("/vanasonad", (req, res)=>{
+	let folkWisdom = [];
+	fs.readFile("public/textfiles/vanasonad.txt", "utf8", (err, data)=>{
+		if(err){
+			throw err;
+		}
+		else {
+			folkWisdom = data.split(";");
+			res.render("justlist", {h2: "Vanasõnad", listData: folkWisdom});
+		}
+	});
+});
+app.listen(5221);
