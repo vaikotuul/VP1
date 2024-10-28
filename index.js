1e11ba34d42104c171f2dbc84df5763daa5c3501
Vaiko Villiam Tuul <vaikotuul@users.noreply.github.com>
2024-10-28 13:34:26 +0200

    Lisatud fotogalerii ja fotode lisamime

--- index.js
+++ index.js
@@ -8,13 +8,19 @@ const bodyparser = require("body-parser");
 const dbInfo = require("../../vp2024config");
 //andmebaasiga suhtlemine
 const mysql = require("mysql2");
+//fotode üleslaadimiseks
+const multer = require("multer");
+//fotomanipulatsiooniks
+const sharp = require("sharp");
 
 //määran view mootori
 app.set("view engine", "ejs");
 //määran jagatavate failide kausta
 app.use(express.static("public"));
 //kasutame body-parserit päringute parsimiseks (kui ainult tekst, siis false, kui ka pildid jms, siis true)
-app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.urlencoded({extended: true}));
+//seadistame fotode üleslaadimiseks vahevara (middleware), mis määrab kataloogi, kuhu laetakse
+const upload = multer({dest: "./public/gallery/orig"});
 
 //loon andmebaasi ühenduse
 const conn = mysql.createConnection({
@@ -144,7 +150,7 @@ app.get("/eestifilm", (req, res)=>{
 });
 
 app.get("/eestifilm/lisa", (req, res)=>{
-	res.render("lisa")
+	res.render("addperson");
 });
 
 app.post("/eestifilm/lisa", (req, res)=>{
@@ -174,7 +180,6 @@ app.post("/eestifilm/lisa", (req, res)=>{
 	res.render("lisa");
 });
 
-
 app.get("/eestifilm/tegelased", (req, res)=>{
 	//loon andmebaasi päringus
 	let sqlReq = "SELECT first_name, last_name, birth_date FROM person";
@@ -191,6 +196,49 @@ app.get("/eestifilm/tegelased", (req, res)=>{
 	//res.render("tegelased");
 });
 
+app.get("/photoupload", (req, res)=>{
+	res.render("photoupload");
+});
 
+app.post("/photoupload", upload.single("photoInput"), (req, res)=>{
+	console.log(req.body);
+	console.log(req.file);
+	const fileName = "vp_" + Date.now() + ".jpg";
+	fs.rename(req.file.path, req.file.destination + "/" + fileName, (err)=>{
+		console.log("Faili nime muutmise viga: " + err);
+	});
+	sharp(req.file.destination + "/" + fileName).resize(800,600).jpeg({quality: 90}).toFile("./public/gallery/normal/" + fileName);
+	sharp(req.file.destination + "/" + fileName).resize(100,100).jpeg({quality: 90}).toFile("./public/gallery/thumb/" + fileName);
+	//salvestame info andmebaasi
+	let sqlReq = "INSERT INTO photos (file_name, orig_name, alt_text, privacy, user_id) VALUES(?,?,?,?,?)";
+	const userId = 1;
+	conn.query(sqlReq, [fileName, req.file.originalname, req.body.altInput, req.body.privacyInput, userId], (err, result)=>{
+		if(err){
+			throw(err);
+		}
+		else {
+			res.render("photoupload");
+		}
+	});
+});
+
+app.get("/gallery", (req, res)=>{
+	let sqlReq = "SELECT file_name, alt_text FROM photos WHERE privacy = ? AND deleted IS NULL ORDER BY id DESC";
+	const privacy = 3;
+	let photoList = [];
+	conn.query(sqlReq, [privacy], (err, result)=>{
+		if(err){
+			throw err;
+		}
+		else {
+			console.log(result);
+			for(let i = 0; i < result.length; i ++) {
+				photoList.push({href: "/gallery/thumb/" + result[i].file_name, alt: result[i].alt_text});
+			}
+			res.render("gallery", {listData: photoList});
+		}
+	});
+	//res.render("gallery");
+});
 
 app.listen(5221);
